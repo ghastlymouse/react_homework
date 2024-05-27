ddd01c1e66c8ccaacb7708b636945b4b85490277
무시무시한쥐 <163982251+ghastlymouse@users.noreply.github.com>
2024-05-27 17:10:10 +0900

    1강과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import "./styles.css"
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const handlePlusCount = () => {
+    setCount(count + 1);
+  };
+  const handleMinusCount = () => {
+    setCount(count - 1);
+  };
+  const handleResetCount = () => {
+    setCount(0);
+  };
+
   return (
-    <div>App</div>
+    <>
+      Count : {count} <br />
+      <button className="plusBtn" onClick={handlePlusCount}>+</button>
+      <button className="minusBtn" onClick={handleMinusCount}>-</button>
+      <button className="resetBtn" onClick={handleResetCount}>Reset</button>
+    </>
   )
 }
 
